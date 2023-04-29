3c86e77f85a518c87e50070d0ded3aaa7ec98a81
dait17 <daitr171003@gmail.com>
2023-04-30 00:54:36 +0700

    - init

---
+++ sources/items.py
@@ -0,0 +1,24 @@
+from gameTools import *
+from game import Game
+
+
+class Items:
+    def __init__(self):
+        pass
+
+
+class ItemBulletLevelUp:
+    def __init__(self):
+        self._imgList = []
+        self._imgId = 0
+        self._vel = 4
+
+
+
+
+
+
+
+
+
+
